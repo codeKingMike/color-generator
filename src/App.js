e28749df638c1ac5fadccb71f2bbf5b678451aea
codeKingMike <codekingmike@gmail.com>
2021-04-08 16:29:32 -0700

    making a color generator

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SingleColor from './SingleColor';
+// import Values from 'values.js';
+
+function App() {
+  return (
+    <section>
+      <h2>color generator app</h2>
+    </section>
+
+  );
+}
+
+export default App;
