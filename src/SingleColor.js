6c778b2ca6fd00828c6c2295c058fa5aabbee8b0
codeKingMike <codekingmike@gmail.com>
2021-04-08 18:50:26 -0700

    done with project

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -5,10 +5,22 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
     const [alert, setAlert] = useState(false)
     const bcg = rgb.join(',')
     const hex = rgbToHex(...rgb)
+    const hexValue = `#${hexColor}`
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setAlert(false);
+        }, 2000)
+        return () => clearTimeout(timeout);
+    }, [alert])
     return (
-        <article className={`color ${index > 10 && 'color-light'}`} style={{ backgroundColor: `rgb(${bcg})` }}>
+        <article className={`color ${index > 10 && 'color-light'}`} style={{ backgroundColor: `rgb(${bcg})` }}
+            onClick={() => {
+                setAlert(true);
+                navigator.clipboard.writeText(hexValue)
+            }}>
             <p className='percent-value'>{weight}%</p>
-            <p className="color-value">{hex}</p>
+            <p className="color-value">{hexValue}</p>
+            {alert && <p className='alert'>copied to clipboard</p>}
         </article>
     );
 }
